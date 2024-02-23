33a712c44b5778f5c20dd96809be3b81024cdd71
arifnurrhmnn <arifnur.rhmnn@gmail.com>
2024-02-23 07:13:27 +0700

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
+// const path = require("path");
+// import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  //   sassOptions: {
+  //     includePaths: [path.join(__dirname, "styles")],
+  //   },
+  images: {
+    domains: [
+      "tmsisuzu.co.id",
+      "www.tmsisuzu.co.id",
+      "blog-media.lifepal.co.id",
+      "isuzu-astra.com",
+      "storage-x.sgp1.cdn.digitaloceanspaces.com",
+      "tms-storage.sgp1.digitaloceanspaces.com",
+      "new-storage.sgp1.digitaloceanspaces.com",
+      "youtu.be",
+    ],
+  },
+};
 
 export default nextConfig;
