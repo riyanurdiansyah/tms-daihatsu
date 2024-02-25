cc130f953dfdc779fc79b332a75cf91b314534d1
arifnurrhmnn <arifnur.rhmnn@gmail.com>
2024-02-25 16:28:51 +0700

    add img hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main>
       <h1>TMS DAIHATSU</h1>
+      <img
+        src="/hero-dummy-2.jpeg"
+        alt="Full Screen"
+        style={{ width: "100%", height: "auto" }}
+      />
     </main>
   );
 }
